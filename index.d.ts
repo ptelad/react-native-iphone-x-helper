9484051a5e2e9fe94a339af0be7a38ddce9ee452
Xiaosong Gao <gaoxiaosong06@gmail.com>
2018-09-04 09:52:13 +0800

    Add getBottomSpace to fit safe area in iPhone X.

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 export function isIphoneX(): boolean;
 export function ifIphoneX<T, U>(iphoneXVal: T, regularVal: U): T | U;
 export function getStatusBarHeight(safe?: boolean): number;
+export function getBottomSpace(): number;
