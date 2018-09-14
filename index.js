f9011ecf8d05f8573621d2fbb2d65e1a311d35c4
José Teixeira <joseteixeira@cosmicode.pt>
2018-09-14 12:54:13 +0100

    Added support to new iPhone Models

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ export function isIphoneX() {
         Platform.OS === 'ios' &&
         !Platform.isPad &&
         !Platform.isTVOS &&
-        (dimen.height === 812 || dimen.width === 812)
+		((dimen.height >= 812 && dimen.width >= 375) ||
+		(dimen.height >= 375 && dimen.width >= 812))
     );
 }
 
