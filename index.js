edca42033e54e7fdb3703f1898a1fd0ef306574e
Jasu Koponen <jasu.koponen@hotmail.com>
2020-10-21 12:54:25 +0300

    Support iPhone 12 mini

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ export function isIphoneX() {
         Platform.OS === 'ios' &&
         !Platform.isPad &&
         !Platform.isTVOS &&
-        ((dimen.height === 812 || dimen.width === 812)
+        ((dimen.height === 780 || dimen.width === 780)
+          || (dimen.height === 812 || dimen.width === 812)
           || (dimen.height === 844 || dimen.width === 844)
           || (dimen.height === 896 || dimen.width === 896)
           || (dimen.height === 926 || dimen.width === 926))
