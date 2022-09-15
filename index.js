46021880cf726d3398d40d2da48535167c8fd0d6
floydkim <floyd@soomgo.com>
2022-09-15 14:58:37 +0900

    support iPhone 14 Pro & Pro Max

--- index.js
+++ index.js
@@ -7,10 +7,23 @@ export function isIphoneX() {
         !Platform.isPad &&
         !Platform.isTVOS &&
         ((dimen.height === 780 || dimen.width === 780)
-          || (dimen.height === 812 || dimen.width === 812)
-          || (dimen.height === 844 || dimen.width === 844)
-          || (dimen.height === 896 || dimen.width === 896)
-          || (dimen.height === 926 || dimen.width === 926))
+            || (dimen.height === 812 || dimen.width === 812)
+            || (dimen.height === 844 || dimen.width === 844)
+            || (dimen.height === 896 || dimen.width === 896)
+            || (dimen.height === 926 || dimen.width === 926)
+            || (dimen.height === 852 || dimen.width === 852) // 14 Pro
+            || (dimen.height === 932 || dimen.width === 932)) // 14 Pro Max
+    );
+}
+
+export function hasIsland() {
+    const dimen = Dimensions.get('window');
+    return (
+        Platform.OS === 'ios' &&
+        !Platform.isPad &&
+        !Platform.isTVOS &&
+        ((dimen.height === 852 || dimen.width === 852) // 14 Pro
+            || (dimen.height === 932 || dimen.width === 932)) // 14 Pro Max
     );
 }
 
@@ -22,8 +35,12 @@ export function ifIphoneX(iphoneXStyle, regularStyle) {
 }
 
 export function getStatusBarHeight(safe) {
+    function safeHeight(hasIsland) {
+        // FIXME: There are more height values depending on the model (iPhone 12/13 -> 47, 13 mini -> 50, ..)
+        return hasIsland ? 59 : 44
+    }
     return Platform.select({
-        ios: ifIphoneX(safe ? 44 : 30, 20),
+        ios: ifIphoneX(safe ? safeHeight(hasIsland()) : 30, 20),
         android: StatusBar.currentHeight,
         default: 0
     });
