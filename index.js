32a5711cb20f819dbe6389fa56b87bbc57f7b857
floydkim <for12gm@gmail.com>
2022-09-16 21:47:22 +0900

    refactor: deduplicate

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import { Dimensions, Platform, StatusBar } from 'react-native';
 
+function isIphone() {
+    return Platform.OS === 'ios' && !Platform.isPad && !Platform.isTVOS;
+}
+
 export function isIphoneX() {
     const dimen = Dimensions.get('window');
     return (
-        Platform.OS === 'ios' &&
-        !Platform.isPad &&
-        !Platform.isTVOS &&
+        isIphone() &&
         ((dimen.height === 780 || dimen.width === 780)
             || (dimen.height === 812 || dimen.width === 812)
             || (dimen.height === 844 || dimen.width === 844)
@@ -19,9 +21,7 @@ export function isIphoneX() {
 export function hasIsland() {
     const dimen = Dimensions.get('window');
     return (
-        Platform.OS === 'ios' &&
-        !Platform.isPad &&
-        !Platform.isTVOS &&
+        isIphone() &&
         ((dimen.height === 852 || dimen.width === 852) // 14 Pro
             || (dimen.height === 932 || dimen.width === 932)) // 14 Pro Max
     );
